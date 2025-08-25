07d534fa7143bb6cbac9366c46e568c02c75bbe6
Daniel Milani <daniel.milani.71@gmail.com>
2025-08-25 13:01:25 -0400

    Lesson 8 update

--- BC_8_RandomNumbers/src/BC_8_RandomNumbers.java
+++ BC_8_RandomNumbers/src/BC_8_RandomNumbers.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
 public class BC_8_RandomNumbers {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Random rand = new Random();
+
+        int number1;
+        double number2;
+        boolean isHeads;
+
+        number1 = rand.nextInt(1,101);
+        number2 = rand.nextDouble(1,101);
+        isHeads = rand.nextBoolean();
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(isHeads);
+
+        if(isHeads){
+            System.out.println("Heads");
+        }
+        else{
+            System.out.println("Tails");
+        }
     }
 }
