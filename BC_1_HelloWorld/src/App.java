78c16cd7754b80e75bf9e1b3e0aa02bb36312795
Daniel Milani <daniel.milani.71@gmail.com>
2025-08-25 11:57:14 -0400

    Lesson 1

---
+++ BC_1_HelloWorld/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+        // THIS is my first java program.
+        /*
+        This is a multi line comment.
+         */
+
+        System.out.print("I like steak!\n");
+        System.out.println("It's really good");
+        System.out.println("Buy me pizza!");
+        
+
+
+
+
+    }
+}
