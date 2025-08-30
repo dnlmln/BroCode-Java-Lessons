a220b752fe6191ba7d56c1420a958f0db7a67408
Daniel Milani <daniel.milani.71@gmail.com>
2025-08-30 10:57:53 -0400

    adding comments

--- BC_19_CalculatorProgram_IfStatement_Basic/src/BC_19_CalculatorProgram_IfStatement_Basic.java
+++ BC_19_CalculatorProgram_IfStatement_Basic/src/BC_19_CalculatorProgram_IfStatement_Basic.java
@@ -3,11 +3,13 @@ public class BC_19_CalculatorProgram_IfStatement_Basic {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
+        // Declaring variables
         double firstNumber = 0;
         double secondNumber = 0;
         double result = 0;
         char operator;
 
+        // Assigning to variables what is input by user.
         System.out.print("Enter the first number: ");
         firstNumber = sc.nextDouble();
 
@@ -17,6 +19,7 @@ public class BC_19_CalculatorProgram_IfStatement_Basic {
         System.out.print("Enter the operator (+, -, *, /): ");
         operator = sc.next().charAt(0);
 
+        // Running operations
         if(operator == '+'){
             result = firstNumber + secondNumber;
         }
@@ -32,7 +35,8 @@ public class BC_19_CalculatorProgram_IfStatement_Basic {
         else{
             System.out.println("Invalid Operator.");
         }
-
+        
+        // Displaying results
         System.out.printf("Operation:      %f %c %f \n", firstNumber, operator, secondNumber);
         System.out.println("The result is:  " + result);
 
