e80f6a869b92c61cdccc25c58c13a31b5f704958
Daniel Milani <daniel.milani.71@gmail.com>
2025-09-09 12:51:12 -0400

    Lesson 32

--- BC_32_ArrayUserInputLesson/src/BC_32_ArrayUserInputLesson.java
+++ BC_32_ArrayUserInputLesson/src/BC_32_ArrayUserInputLesson.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Scanner;
 public class BC_32_ArrayUserInputLesson {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        // array with 3 elements
+        //String[] foods = {"pizza", "taco", "hamburger"};
+
+        // empty array that has space for 3 values
+        String[] foods = new String[3];
+
+        //
+        //foods[0] = "Pizza";
+        //foods[1] = "taco";
+        //foods[2] = "hamburger"
+
+        //System.out.print("What is the first food: ");
+        //foods[0] = sc.nextLine();
+        //System.out.print("What is the second food: ");
+        //foods[1] = sc.nextLine();
+        //System.out.print("What is the third food: ");
+        //foods[2] = sc.nextLine();
+
+        for(int i = 0; i < foods.length; i++){
+            System.out.print("Enter your food: ");
+            foods[i] = sc.nextLine();
         }
+
+
+        System.out.println("You have " + foods.length + " elements in your array.");
+        Arrays.sort(foods);
+
+        for(String food : foods){
+            System.out.print(food + " ");
+        }
+
+        sc.close();
     }
 }
